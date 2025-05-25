30b6d0a941fda72023a876c32a738090c71f99bf
Haidvhe181849 <danghaix27@gmail.com>
2025-05-25 23:01:05 +0700

    Commit đầu tiên

---
+++ src/java/Utility/DBContext.java
@@ -0,0 +1,62 @@
+package Utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+        //@Students: You are allowed to edit user, pass, url variables to fit 
+        //your system configuration
+        //You can also add more methods for Database Interaction tasks. 
+        //But we recommend you to do it in another class
+        // For example : StudentDBContext extends DBContext , 
+        //where StudentDBContext is located in dal package, 
+        try {
+            String user = "sa";
+            String pass = "123";
+            String url = "jdbc:sqlserver://LAPTOP-02GNNTSP:1433;databaseName=PetHospital";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public ResultSet getData(String sql){
+        ResultSet rs = null;
+        Statement st;
+        try {
+            st = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            rs=st.executeQuery(sql);
+        } catch (SQLException ex) {
+            ex.getStackTrace();
+        }       
+       return rs; 
+    }
+    
+    public boolean isConnected() {
+        try {
+            return connection != null && !connection.isClosed();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    // Phương thức main để kiểm tra kết nối
+    public static void main(String[] args) {
+        DBContext dbContext = new DBContext();
+        if (dbContext.isConnected()) {
+            System.out.println("Kết nối cơ sở dữ liệu thành công!");
+        } else {
+            System.out.println("Kết nối cơ sở dữ liệu thất bại.");
+        }
+
+    }
+}
